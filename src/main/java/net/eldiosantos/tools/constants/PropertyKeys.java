503dd9c852da31d7a4f1702b34f3e5e7c16435ba
Eldius <eldiosantos@gmail.com>
2015-07-07 12:14:11 -0300

    Added 2 modules to test 2 diferent ways to use the lib.

--- src/main/java/net/eldiosantos/tools/constants/PropertyKeys.java
+++
@@ -1,9 +0,0 @@
-package net.eldiosantos.tools.constants;
-
-/**
- * Created by Eldius on 05/07/2015.
- */
-public class PropertyKeys {
-    public static final String ROOT_PATH = "net.eldiosantos.jndi.root";
-    public static final String CONTEXT_FACTORY = "java.naming.factory.initial";
-}
