7eb958c7235746ab2e84a69fd44aebfcde21ad92
Eldius <eldiosantos@gmail.com>
2015-07-28 15:36:25 -0300

    Changes to test script to show a success status after the execution.

--- run.sh
+++ run.sh
@@ -10,4 +10,5 @@ java -jar light-jndi-customfactory-test/target/light-jndi-customfactory-test-jar
 echo Second test && \
 java -jar light-jndi-embedded-test/target/light-jndi-embedded-test-jar-with-dependencies.jar && \
 echo Thirsd test && \
-java -jar light-jndi-test/target/light-jndi-test-jar-with-dependencies.jar
+java -jar light-jndi-test/target/light-jndi-test-jar-with-dependencies.jar && \
+echo Finished with success.
